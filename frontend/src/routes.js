09c90e78248e5814be5f3abaff86307398060076
DNAYMUSHIN <denisnaymushin05@mail.ru>
2025-06-24 14:49:22 +0300

    Чайная лавка

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,6 @@
 import {LOGIN_ROUTE, CRM_ROUTE} from "./utils/consts";
 import CRM from "./pages/CRM";
 import Login from "./pages/Login.jsx";
-/*import {createBrowserRouter} from "react-router-dom";
-
-export const authRoutes = createBrowserRouter([
-    {
-        path: CRM_ROUTE,
-        Component: CRM
-    },
-    {
-        path: LOGIN_ROUTE,
-        Component: Login
-    },
-])*/
 
 export const authRoutes = [
     {
@@ -27,3 +15,16 @@ export const publicRoutes = [
         Component: Login
     },
 ]
+
+/*import {createBrowserRouter} from "react-router-dom";
+
+export const authRoutes = createBrowserRouter([
+    {
+        path: CRM_ROUTE,
+        Component: CRM
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Login
+    },
+])*/
